be116edef1cc6c432b51befaf424fe7473e464e9
rui-314 <87481129+rui-314@users.noreply.github.com>
2022-05-05 10:15:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -12,6 +12,21 @@
         });
     }
 
+    function onScrollCallback(entries, obs) {
+        entries.forEach( entry => {
+            if(!entry.isIntersecting){
+                header.classList.add('scrolled');
+                toTop.classList.add('scrolled');
+            }else{
+                header.classList.remove('scrolled');
+                toTop.classList.remove('scrolled');
+            }
+        })
+    }
+
+    const header = document.querySelector('header');
+    const toTop = document.getElementById('nav')
+
     const inViewObserver = new IntersectionObserver(inViewCallback, {
         threshold: 0.2,
     });
@@ -19,4 +34,8 @@
     document.querySelectorAll('.target').forEach(el => {
         inViewObserver.observe(el);
     });
+
+    const onScrollObserver = new IntersectionObserver(onScrollCallback);
+
+    onScrollObserver.observe(document.getElementById('scroll_target'));
 }
